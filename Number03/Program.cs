75452220b3ee4bb9554dc4703f11679efd423e9b
Svetlana <medjog@gmail.com>
2023-03-24 16:32:52 +0400

    Написана программа для задачи 3

--- Number03/Program.cs
+++ Number03/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.Clear();
+Console.WriteLine("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.WriteLine($"Число {a} - чётное.");
+}
+else
+{
+    Console.WriteLine($"Число {a} - нечётное.");
+}
