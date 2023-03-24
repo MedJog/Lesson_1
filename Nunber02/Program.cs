2cec19af4d739cdb40eaf79ddc2b2c01eca533d5
Svetlana <medjog@gmail.com>
2023-03-24 16:11:49 +0400

    Написана программа для задачи 2

--- Nunber02/Program.cs
+++ Nunber02/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Clear();
+Console.WriteLine("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число c: ");
+int c = int.Parse(Console.ReadLine());
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+
+if (c > max)
+{
+  max = c;
+}
+
+Console.WriteLine($"Максимальное из этих чисел {max}");
+
